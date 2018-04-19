8aedc4620d324417659d0f8aa044abaacf801d65
Yusuf Alakram <ya328@bath.ac.uk>
2018-04-19 18:17:19 +0100

    q learning algo done

--- QLearning.py
+++ QLearning.py
@@ -10,21 +10,51 @@ QTable = dict()
 
 def qLearn(epsilon, alpha, episodes):
     # Repeat for each episode
-    for _ in range(episodes):
+    for i in range(episodes):
         env = newEnv()
-        currState = env.grid
+        env.act(action=agentMove(env))
+        currState = str(env.grid)
 
-        while (not env.grid_is_full()) and (not env.was_winning_move()):
+        while True:
+            env.change_turn()
             availableActions = env.available_actions
             selectedAction = greedyChooseAction(epsilon, availableActions, currState)
             env.act(action=selectedAction)
-            reward = 1 if (env.was_winning_move()) else 0
-            env = agentMove(env)
-            QTable[currState, selectedAction] = ((1-alpha) * QTable[currState, selectedAction]) + ((alpha) * (reward + maxQ(env.available_actions, env.grid)))
-            currState = env.grid
+
+            if (env.was_winning_move()):
+                reward = 1
+                QTable[currState, selectedAction] = ((1 - alpha) * QTable[currState, selectedAction]) + (
+                            (alpha) * (reward + maxQ(env.available_actions, str(env.grid))))
+                break
+            elif env.grid_is_full():
+                reward = 0
+                QTable[currState, selectedAction] = ((1 - alpha) * QTable[currState, selectedAction]) + (
+                            (alpha) * (reward + maxQ(env.available_actions, str(env.grid))))
+                break
+            else:
+                env.change_turn()
+                env.act(action=agentMove(env))
+                if env.was_winning_move():
+                    reward = -1
+                    QTable[currState, selectedAction] = ((1 - alpha) * QTable[currState, selectedAction]) + (
+                                (alpha) * (reward + maxQ(env.available_actions, str(env.grid))))
+                    break
+                elif env.grid_is_full():
+                    reward = 0
+                    QTable[currState, selectedAction] = ((1 - alpha) * QTable[currState, selectedAction]) + (
+                                (alpha) * (reward + maxQ(env.available_actions, str(env.grid))))
+                    break
+                else:
+                    reward = 0
+                    QTable[currState, selectedAction] = ((1 - alpha) * QTable[currState, selectedAction]) + (
+                                (alpha) * (reward + maxQ(env.available_actions, str(env.grid))))
+                    currState = str(env.grid)
 
 
 def greedyChooseAction(epsilon, availableActions, state):
+    if (len(availableActions) == 1):
+        return availableActions[0]
+
     # Create sorted list of pairs of type (action, value)
     actionValues = []
 
@@ -40,9 +70,9 @@ def greedyChooseAction(epsilon, availableActions, state):
 
     # Pick the best action, or on occasion, another random action
     if random.random() < epsilon:
-        chosenAction = actionValues[random.randint(1, len(actionValues)-1)][0]
+        chosenAction = (actionValues[random.randint(1, len(actionValues)-1)])[0]
     else:
-        chosenAction = actionValues[0][0]
+        chosenAction = (actionValues[0])[0]
     return chosenAction
 
 
@@ -59,16 +89,18 @@ def maxQ(availableActions, state):
     # Sort this list based on the value of the action, in descending order
     actionValues.sort(key=operator.itemgetter(1), reverse=True)
 
-    # Pick the value of the best action
-    bestActionValue = actionValues[0][1]
+    if (len(actionValues) == 0):
+        return 0
+    else:
+        # Pick the value of the best action
+        bestActionValue = (actionValues[0])[1]
 
-    return bestActionValue
+        return bestActionValue
 
 
 def newEnv():
     env = connect.Connect(verbose=True)
     env.reset(first_player='x')
-    env = agentMove(env)
     return env
 
 
@@ -76,9 +108,7 @@ def agentMove(env):
     availableActions = env.available_actions
     selectedActionIndex = random.randint(0,len(availableActions)-1)
     selectedAction = availableActions[selectedActionIndex]
-    env.act(action=selectedAction)
-    env.change_turn()
-    return env
+    return selectedAction
 
 
-qLearn(0.2, 0.1, 10)
+qLearn(0.2, 0.6, 1000)
